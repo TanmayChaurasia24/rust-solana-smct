e794898c5c16c7c13a18a43fb2e071e1239c3222
tanmay <tanmaykumar.kanpur@gmail.com>
2025-04-22 21:47:31 +0530

    ownership: done till .clone()

--- src/main.rs
+++ src/main.rs
@@ -37,8 +37,43 @@ fn main() {
     // --------------------functions---------------------
     let result: i32 = add(10, 20);
     println!("result is: {}", result);
+
+    ownership_example();
 }
 
 fn add(a: i32, b: i32) -> i32 {
     return a + b;
+}
+
+fn ownership_example() {
+    let a: i8 = 1;
+    let b: i8 = a;
+
+    println!("a: {}",a);
+    println!("b: {}",b);
+
+    // this code will give error because ownership rules are getting violated
+    let str1:String = String::from("tanmay"); // str1 is the owner of tanmay
+    let str2:String = str1; // we are transfering the ownership of str1 to str2, in rust when we transfer the ownership of one variable to another then the fist owner 
+    // println!("str1: {}", str1);
+    println!("str2: {}", str2);
+
+    let x:String = String::from("tanmay kumar chaurasia"); // x is the pointer and is the owner of tanmay kumar chaurasia
+    process_string(x); // trasfering the owner ship to s and x is now not the owner of tanmay kumar chaurasia
+    // println!("the value of x is {}", x); // this will give error as x is now not the owner of anything string
+
+    // example for transfering onwer ship and returning many things from function
+    let name: String = String::from("tanmay");
+    // let (s2,length) = cal_length(name); // this is transfering the ownership of the name
+    let length: usize = cal_length(name.clone()); // we are not transfering the ownership of name, instead we are just passing the clone of name, so that we are also access name
+    println!("the length of {} is {}",name,length);
+}
+
+fn cal_length(s: String) -> usize{
+    let length: usize = s.len();
+    return length;
+}
+
+fn process_string(s: String) {
+    println!("processing string: {}", s); // this will print tanmay kumar chaurasia
 }
