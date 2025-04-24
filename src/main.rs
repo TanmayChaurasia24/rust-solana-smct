2c3c3fdc95488df431501351b7ae6d28e64b559c
tanmay <tanmaykumar.kanpur@gmail.com>
2025-04-24 22:27:35 +0530

    basic rust done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,129 @@
+use std::default;
+use std::io;
+
 fn main() {
+    rust_basic();
+    ownership_example();
+    reference_and_derefrence();
+    arrays_example();
+    dynamic_array_vector();
+    shadowing_rust();
+    match_rust();
+    io_rust();
+}
+
+fn io_rust() {
+    let mut name: String = String::new();
+    io::stdin().read_line(&mut name).expect("input failed");
+    println!("name os user is: {}", name);
+}
+
+fn match_rust() {
+    let number: i8 = 20;
+
+    match number {
+        1 | 3 => println!("number is 1"),
+        10 | 20 => println!("number is 10"),
+        5 => println!("number is 5"),
+        _ => println!("not a valid number"),
+    }
+}
+
+fn shadowing_rust() {
+    // this is allowed in rust
+
+    let x = 5;
+    println!("x={}", x);
+    let x = "tanmay kumar charuasia";
+    println!("x={}", x);
+    let x = x.len();
+    println!("x={}", x);
+}
+
+fn dynamic_array_vector() {
+    let mut v: Vec<i8> = Vec::new(); // vector declaration
+    let mut nums = Vec::<String>::new(); // another way of declaring vector
+
+    nums.push(String::from("tanmay"));
+    nums.push(String::from("kumar"));
+    nums.push(String::from("charuasia"));
+
+    v.push(1);
+    v.push(2);
+    v.push(3);
+
+    let mut nums1 = vec![1, 2, 3, 4, 5, 6]; // initializing the vector with the values
+    nums1.push(10);
+
+    println!("value of vector v is: {:?}", v);
+    println!("value of vector nums is: {:?}", nums); // value of vector nums is: ["tanmay", "kumar", "charuasia"]
+    println!("value of vector nums1 is: {:?}", nums1); //value of vector nums1 is: [1, 2, 3, 4, 5, 6, 10]
+}
+
+fn arrays_example() {
+    // let arr: [&str;3] = ["hello","world","chase"];
+    // write_arr(arr); // when we have passed arr directly then arr1 is thw new copy of arr thats why after changes to arr1 arr is still same
+    let mut arr: [&str; 3] = ["hello", "world", "chase"];
+    write_arr(&mut arr); // passing the reference of arr now changes to arr1 will be shown to arr also
+    println!("arr = {:?}", arr);
+}
+
+fn write_arr(arr1: &mut [&str; 3]) {
+    arr1[0] = "fellow";
+    println!("arr1 = {:?}", arr1)
+}
+
+fn reference_and_derefrence() {
+    let mut x: i8 = 5;
+    x = x + 1; //6
+    let y: &mut i8 = &mut x; // y is the reference to the value of x, y is storing the address of x *y will access the value of x.
+    *y += 1; //7 ------ derefrencing 
+    println!("x = {}", y);
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    return a + b;
+}
+
+fn ownership_example() {
+    let a: i8 = 1;
+    let b: i8 = a;
+
+    println!("a: {}", a);
+    println!("b: {}", b);
+
+    // this code will give error because ownership rules are getting violated
+    let str1: String = String::from("tanmay"); // str1 is the owner of tanmay
+    let str2: String = str1; // we are transfering the ownership of str1 to str2, in rust when we transfer the ownership of one variable to another then the fist owner 
+    // println!("str1: {}", str1);
+    println!("str2: {}", str2);
+
+    let x: String = String::from("tanmay kumar chaurasia"); // x is the pointer and is the owner of tanmay kumar chaurasia
+    process_string(x); // trasfering the owner ship to s and x is now not the owner of tanmay kumar chaurasia
+    // println!("the value of x is {}", x); // this will give error as x is now not the owner of anything string
+
+    // example for transfering onwer ship and returning many things from function
+    let name: String = String::from("tanmay");
+    // let (s2,length) = cal_length(name); // this is transfering the ownership of the name
+
+    // clone method is used for deep copy of the heap data. it is an expensive method
+    // let length: usize = cal_length(name.clone()); // we are not transfering the ownership of name, instead we are just passing the clone of name, so that we are also access name
+
+    //-----------borrowing---------------
+    let length: usize = cal_length(&name);
+    println!("the length of {} is {}", name, length);
+}
+
+fn cal_length(s: &String) -> usize {
+    let length: usize = s.len();
+    return length;
+}
+
+fn process_string(s: String) {
+    println!("processing string: {}", s); // this will print tanmay kumar chaurasia
+}
+
+fn rust_basic() {
     let num: u16 = 20;
     println!("value of num is: {}", num);
     // num = 199 // this will give an error because num is inmutable
@@ -37,72 +162,4 @@ fn main() {
     // --------------------functions---------------------
     let result: i32 = add(10, 20);
     println!("result is: {}", result);
-
-    // ownership_example();
-    // reference_and_derefrence();
-    arrays_example();
-}
-
-fn arrays_example() {
-    // let arr: [&str;3] = ["hello","world","chase"];
-    // write_arr(arr); // when we have passed arr directly then arr1 is thw new copy of arr thats why after changes to arr1 arr is still same
-    let mut arr: [&str;3] = ["hello","world","chase"];
-    write_arr(&mut arr); // passing the reference of arr now changes to arr1 will be shown to arr also
-    println!("arr = {:?}", arr);
-}
-
-fn write_arr(arr1:&mut [&str;3]) {
-    arr1[0] = "fellow";
-    println!("arr1 = {:?}", arr1)
-}
-
-fn reference_and_derefrence() {
-    let mut x: i8 = 5;
-    x = x+1; //6
-    let y: &mut i8 = &mut x; // y is the reference to the value of x, y is storing the address of x *y will access the value of x.
-    *y += 1; //7 ------ derefrencing 
-    println!("x = {}", y);
-
-}
-
-fn add(a: i32, b: i32) -> i32 {
-    return a + b;
-}
-
-fn ownership_example() {
-    let a: i8 = 1;
-    let b: i8 = a;
-
-    println!("a: {}",a);
-    println!("b: {}",b);
-
-    // this code will give error because ownership rules are getting violated
-    let str1:String = String::from("tanmay"); // str1 is the owner of tanmay
-    let str2:String = str1; // we are transfering the ownership of str1 to str2, in rust when we transfer the ownership of one variable to another then the fist owner 
-    // println!("str1: {}", str1);
-    println!("str2: {}", str2);
-
-    let x:String = String::from("tanmay kumar chaurasia"); // x is the pointer and is the owner of tanmay kumar chaurasia
-    process_string(x); // trasfering the owner ship to s and x is now not the owner of tanmay kumar chaurasia
-    // println!("the value of x is {}", x); // this will give error as x is now not the owner of anything string
-
-    // example for transfering onwer ship and returning many things from function
-    let name: String = String::from("tanmay");
-    // let (s2,length) = cal_length(name); // this is transfering the ownership of the name
-
-    // clone method is used for deep copy of the heap data. it is an expensive method
-    // let length: usize = cal_length(name.clone()); // we are not transfering the ownership of name, instead we are just passing the clone of name, so that we are also access name
-
-    //-----------borrowing---------------
-    let length: usize = cal_length(&name);
-    println!("the length of {} is {}",name,length);
-}
-
-fn cal_length(s: &String) -> usize{
-    let length: usize = s.len();
-    return length;
-}
-
-fn process_string(s: String) {
-    println!("processing string: {}", s); // this will print tanmay kumar chaurasia
 }
